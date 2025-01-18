b4f2bf323b658955d2e0dd5e3983b18bc75123a2
GioChkhaidze <giochkhaidze10@gmail.com>
2025-01-18 10:55:50 +0300

    Add login & register pages & sample navbar

---
+++ chat/pages/login.py
@@ -0,0 +1,93 @@
+import reflex as rx
+from chat.components.mainNavbar import mainNavbar
+
+def login() -> rx.Component:
+    return rx.card(
+        rx.vstack(
+            rx.center(
+                # rx.image(
+                #     src="/logo.jpg",
+                #     width="2.5em",
+                #     height="auto",
+                #     border_radius="25%",
+                # ),
+                rx.heading(
+                    "Sign in to your account",
+                    size="6",
+                    as_="h2",
+                    text_align="center",
+                    width="100%",
+                ),
+                direction="column",
+                spacing="5",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Email address",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    placeholder="user@reflex.dev",
+                    type="email",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.hstack(
+                    rx.text(
+                        "Password",
+                        size="3",
+                        weight="medium",
+                    ),
+                    # rx.link(
+                    #     "Forgot password?",
+                    #     href="#",
+                    #     size="3",
+                    # ),
+                    justify="between",
+                    width="100%",
+                ),
+                rx.input(
+                    placeholder="Enter your password",
+                    type="password",
+                    size="3",
+                    width="100%",
+                ),
+                spacing="2",
+                width="100%",
+            ),
+            rx.button("Sign in", size="3", width="100%"),
+            rx.center(
+                rx.text("New here?", size="3"),
+                rx.link("Sign up", href="/register", size="3"),
+                opacity="0.8",
+                spacing="2",
+                direction="row",
+            ),
+            spacing="6",
+            width="100%",
+        ),
+        size="4",
+        max_width="28em",
+        width="100%",
+    )
+
+def LoginPage():
+    return rx.vstack(
+        mainNavbar(),
+        login(),
+        #flex="1",  
+        #width="100%",
+        align_items="center",
+        #justify_content="center",
+        #background_color="gray.100",
+        spacing="8"
+    )
